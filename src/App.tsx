c8b915cf1a3fc58d63b5bbaefdc2d5cb4fad5485
Jerry-Jzy <jzyswufe@163.com>
2023-04-06 19:50:28 -0400

    add name

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ function App(): JSX.Element {
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript
             </header>
-            <header className="App-header">Zachary Witte</header>
+            <header className="App-header">Zachary Witte, ZhongYiJiang</header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
                 automatically reload.
