418dee3f7dc2dce2eafc44e38921302c740b65c1
Grace Melton <gmelton@udel.edu>
2023-04-06 21:23:41 -0400

    added Grace Melton

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,7 @@ function App(): JSX.Element {
         <div className="App">
             <header className="App-header">
                 UD CISC275 with React Hooks and TypeScript Rachel Sison, Gia
-                Diaz, Steven Kraus,
+                Diaz, Steven Kraus, Grace Melton
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
