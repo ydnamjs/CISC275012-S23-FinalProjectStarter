9827bf48c12954342ec2d66d565bbd832e1f99f1
rachel-sison <RSISON@UDEL.EDU>
2023-04-06 21:17:07 -0400

    added name

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,10 @@
-import React from "react";
 import "./App.css";
 
 function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
+                UD CISC275 with React Hooks and TypeScript Rachel Sison
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
