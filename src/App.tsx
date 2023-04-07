7b4aa68f08be2bee23369d30fdd17d189de4b15a
gdiaz18 <giadiaz@udel.edu>
2023-04-06 21:19:05 -0400

    app

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,8 @@ function App(): JSX.Element {
     return (
         <div className="App">
             <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript Rachel Sison
+                UD CISC275 with React Hooks and TypeScript Rachel Sison, Gia
+                Diaz,
             </header>
             <p>
                 Edit <code>src/App.tsx</code> and save. This page will
