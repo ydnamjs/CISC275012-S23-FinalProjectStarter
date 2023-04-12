28abb7e7faf7ac19214a70eb035ff805e8330292
Steven <skraus@udel.edu>
2023-04-12 11:31:04 -0400

    shirt display

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import React from "react";
-import { Button } from "react-bootstrap";
-import { Col, Container, Row } from "react-bootstrap";
+import { Button, Col, Container, Row } from "react-bootstrap";
 import shirt1 from "./images/shirt1.png";
 import shirt2 from "./images/shirt2.png";
 import sweatshirt1 from "./images/sweatshirt1.png";
 import sweatshirt2 from "./images/sweatshirt2.png";
 import "./App.css";
-import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
