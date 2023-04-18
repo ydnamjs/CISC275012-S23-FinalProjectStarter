cc47834212618ed8afa76e9c1f9abd2cf14f85b2
KEVIN CHAU <kevinch@udel.edu>
2023-04-18 11:33:50 -0400

    added shop button

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,6 +5,9 @@ import runners from "../assets/runners.png";
 const FrontPage = () => {
     return (
         <div className="image1">
+            <button type="button" className="shop-now">
+                Shop Now
+            </button>
             <img src={runners}></img>
             <h1 className="text">EFFORTLESS</h1>
             <p className="description">Redefining Athletic Wear</p>
