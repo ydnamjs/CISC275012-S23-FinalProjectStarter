196f02c0838d1f52acac71926f3e9d44c64f385d
KEVIN CHAU <kevinch@udel.edu>
2023-04-19 14:32:12 -0400

    testing production deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import FrontPage from "./components/Main";
 import "./App.css";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import MenTops from "./components/pages/MenTops";
 import MenShorts from "./components/pages/MenShorts";
 import MenPants from "./components/pages/MenPants";
