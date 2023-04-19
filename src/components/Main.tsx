11a9d805da666896588f2c2e7a6cea1f0aa93696
BYeungCoding <bluefroggy321@icloud.com>
2023-04-19 10:43:01 -0400

    updated my branch

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,15 +8,6 @@ const FrontPage = () => {
             <img src={runners}></img>
             <h1 className="text">E F F O R T L E S S</h1>
             <p className="description">Redefining Athletic Wear</p>
-<<<<<<< HEAD
-            <a href="http://google.com" className="shop-now">
-                Shop Now
-            </a>
-            <a href="http://google.com" className="shop-now">
-                Shop Now
-            </a>
-=======
->>>>>>> 35f35ff5df8de7786dc6c32d5c45c6f625310e6e
         </div>
     );
 };
