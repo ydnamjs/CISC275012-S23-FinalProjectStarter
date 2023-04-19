096e29e62f5b8dbfdd69b0ab962da7328b528c22
KEVIN CHAU <kevinch@udel.edu>
2023-04-19 02:01:19 -0400

    added new stuff

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -8,14 +8,8 @@ const FrontPage = () => {
             <img src={runners}></img>
             <h1 className="text">E F F O R T L E S S</h1>
             <p className="description">Redefining Athletic Wear</p>
-            <form
-                className="shop-now"
-                method="GET"
-                action="https://www.google.com"
-            >
-                <input type="submit" value="Shop Now" />
-            </form>
         </div>
     );
 };
+
 export default FrontPage;
