1273bbaf2f3dd7581df111b3a1356de2159167c3
MikeMcCully <mikemnj@udel.edu>
2023-04-20 00:51:27 -0400

    ProductListMen added to mens page

---
+++ src/components/ProductListMen.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import Product from "./Product";
+import "./ProductListMen.css";
+
+const products = [
+    {
+        name: "Product 1",
+        price: 10,
+        description: "Lorem ipsum dolor sit amet.",
+        sizes: ["Small", "Medium", "Large"],
+        picture: "ud_shirt"
+    },
+    {
+        name: "Product 2",
+        price: 20,
+        description: "Consectetur adipiscing elit.",
+        sizes: ["Small", "Large"],
+        picture: "ud_shirt"
+    },
+    {
+        name: "Product 3",
+        price: 30,
+        description: "Nullam accumsan cursus semper.",
+        sizes: ["Medium"],
+        picture: "ud_shirt"
+    }
+];
+
+const ProductList = () => {
+    return (
+        <div className="productList">
+            {products.map((product) => (
+                <Product {...product} key={product.name} />
+            ))}
+        </div>
+    );
+};
+
+export default ProductList;
--- src/components/pages/Mens.tsx
+++ src/components/pages/Mens.tsx
@@ -1,11 +1,13 @@
 import React from "react";
 import NavBar from "../Navbar";
+import ProductListMen from "../ProductListMen";
 
 const Mens = () => {
     return (
         <div>
             <NavBar />
             Mens clothing
+            <ProductListMen />
         </div>
     );
 };
