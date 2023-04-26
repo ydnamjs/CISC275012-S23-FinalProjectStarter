d0686665cb1582955f9504a02e86929d6a385573
KEVIN CHAU <kevinch@udel.edu>
2023-04-26 03:57:59 -0400

    stashing mens

--- src/components/pages/Mens.tsx
+++ src/components/pages/Mens.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import NavBar from "../Navbar";
+import "../pages/Mens.css";
+import UDshirt from "../../assets/ud_shirt.png";
+import Product from "../Product";
 
 const Mens = () => {
     return (
-        <div>
+        <div className="background">
             <NavBar />
-            Mens clothing
+            <img className="product-image" src={UDshirt}></img>
+            <h1 className="background">THIS IS MENS PAGE</h1>
+            Mens clothing Bello There
         </div>
     );
 };
