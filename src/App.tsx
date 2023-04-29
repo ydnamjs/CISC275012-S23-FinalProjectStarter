833c5019b82d200bef969a74e44f39d172d48ca4
KEVIN CHAU <kevinch@udel.edu>
2023-04-29 14:59:19 -0400

    added shopping cart functionality

--- src/App.tsx
+++ src/App.tsx
@@ -4,20 +4,23 @@ import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Mens from "./components/pages/Mens";
 import Womens from "./components/pages/Womens";
+import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 
 function App(): JSX.Element {
     return (
-        <div>
-            <React.StrictMode>
-                <Router>
-                    <Routes>
-                        <Route path="/" element={<FrontPage />} />
-                        <Route path="/men" element={<Mens />} />
-                        <Route path="/women" element={<Womens />} />
-                    </Routes>
-                </Router>
-            </React.StrictMode>
-        </div>
+        <ShoppingCartProvider>
+            <div>
+                <React.StrictMode>
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={<FrontPage />} />
+                            <Route path="/men" element={<Mens />} />
+                            <Route path="/women" element={<Womens />} />
+                        </Routes>
+                    </Router>
+                </React.StrictMode>
+            </div>
+        </ShoppingCartProvider>
     );
 }
 
