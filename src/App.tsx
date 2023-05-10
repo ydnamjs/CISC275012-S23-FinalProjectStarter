8a82c6a58ea8c1fd4cf4aaccf9079234dbb93e13
BYeungCoding <bluefroggy321@icloud.com>
2023-05-10 19:32:12 -0400

    Register/Login

--- src/App.tsx
+++ src/App.tsx
@@ -4,9 +4,11 @@ import "./App.css";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import Mens from "./components/pages/Mens";
 import Admin from "./components/pages/Admin";
-import Login from "./components/pages/Login";
+import Registration from "./components/pages/Registration";
 import { ShoppingCartProvider } from "./context/ShoppingCartContext";
 import CheckoutModal from "./components/Modal";
+import Logout from "./components/Logout";
+import Login from "./components/pages/Login";
 
 function App(): JSX.Element {
     return (
@@ -18,11 +20,16 @@ function App(): JSX.Element {
                             <Route path="/" element={<FrontPage />} />
                             <Route path="/men" element={<Mens />} />
                             <Route path="/admin" element={<Admin />} />
+                            <Route
+                                path="/register"
+                                element={<Registration />}
+                            />
                             <Route path="/login" element={<Login />} />
                             <Route
                                 path="/checkout"
                                 element={<CheckoutModal />}
                             />
+                            <Route path="/logout" element={<Logout />} />
                         </Routes>
                     </Router>
                 </React.StrictMode>
