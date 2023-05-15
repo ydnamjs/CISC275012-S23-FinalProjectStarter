e00e28abe7b76d90863c0379103ae42afb0cbdf3
KEVIN CHAU <kevinch@udel.edu>
2023-05-14 21:33:09 -0400

    ssaving

--- src/components/Modal.tsx
+++ src/components/Modal.tsx
@@ -2,11 +2,8 @@
 import React, { useState } from "react";
 import { Form } from "react-bootstrap";
 import NavBar from "./Navbar";
-import { useShoppingCart } from "../context/ShoppingCartContext";
-import formatMoney from "../utility/formatMoney";
-import { prodM, prodW } from "./ProductList";
-import { CheckoutItem } from "./CheckoutItem";
-import { VStack, Stack, Divider } from "@chakra-ui/react";
+import PaymentInformation from "./PaymentInformation";
+import OrderConfirmation from "./pages/OrderConfirmation";
 
 const usStates: string[] = [
     "Alabama",
@@ -60,26 +57,7 @@ const usStates: string[] = [
     "Wisconsin",
     "Wyoming"
 ];
-const expYears: number[] = [
-    2024, 2025, 2026, 2027, 2028, 2029, 2030, 2031, 2032
-];
-const expMonths: string[] = [
-    "01",
-    "02",
-    "03",
-    "04",
-    "05",
-    "06",
-    "07",
-    "08",
-    "09",
-    "10",
-    "11",
-    "12"
-];
-
 const CheckoutModal = () => {
-    const { cartItems } = useShoppingCart();
     const [Fname, setFName] = useState("");
     const [Lname, setLName] = useState("");
     const [email, setEmail] = useState("");
@@ -89,7 +67,6 @@ const CheckoutModal = () => {
     const [city, setCity] = useState("");
     const [state, setState] = useState("");
     const [zip, setZip] = useState("");
-    const [cardNum, setCardNum] = useState("");
 
     function updateFName(event: React.ChangeEvent<HTMLInputElement>) {
         setFName(event.target.value);
@@ -118,9 +95,6 @@ const CheckoutModal = () => {
     function updateZip(event: React.ChangeEvent<HTMLInputElement>) {
         setZip(event.target.value);
     }
-    function updateCardNum(event: React.ChangeEvent<HTMLInputElement>) {
-        setCardNum(event.target.value);
-    }
 
     return (
         <div>
@@ -132,15 +106,6 @@ const CheckoutModal = () => {
                             <div className="card">
                                 <div className="card-header">
                                     <h4>Shipping Information</h4>
-                                    {email}
-                                    {Fname}
-                                    {Lname}
-                                    {number}
-                                    {addr1}
-                                    {addr2}
-                                    {city}
-                                    {state}
-                                    {zip}
                                 </div>
                                 <div className="card-body">
                                     <div className="row">
@@ -288,155 +253,18 @@ const CheckoutModal = () => {
                     </div>
                 </div>
             </div>
-
-            <div className="py-4">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-6">
-                            <div className="card">
-                                <div className="card-header">
-                                    <h4>Payment Details</h4>
-                                </div>
-                                <div className="card-body">
-                                    <div className="row">
-                                        <div className="col-md-6"></div>
-                                    </div>
-                                    <div className="col-md-14">
-                                        <div className="form-group mb-3">
-                                            <Form.Group>
-                                                <Form.Label>
-                                                    Cardholder Name
-                                                </Form.Label>
-                                                <Form.Control></Form.Control>
-                                            </Form.Group>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-14">
-                                        <div className="form-group mb-3">
-                                            <Form.Group>
-                                                <Form.Label>
-                                                    Card Number
-                                                </Form.Label>
-                                                <Form.Control
-                                                    value={cardNum}
-                                                    onChange={updateCardNum}
-                                                ></Form.Control>
-                                            </Form.Group>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-2">
-                                        <div>
-                                            <Form.Group>
-                                                <Form.Label>
-                                                    Security Code
-                                                </Form.Label>
-                                                <Form.Control></Form.Control>
-                                                <Form.Label>
-                                                    Expiration Month
-                                                </Form.Label>
-                                                <Form.Select>
-                                                    {expMonths.map((month) => (
-                                                        <option
-                                                            key={month}
-                                                            value={month}
-                                                        >
-                                                            {month}
-                                                        </option>
-                                                    ))}
-                                                </Form.Select>
-                                                <Form.Label>
-                                                    Expiration Year
-                                                </Form.Label>
-                                                <Form.Select>
-                                                    {expYears.map((year) => (
-                                                        <option
-                                                            key={year}
-                                                            value={year}
-                                                        >
-                                                            {year}
-                                                        </option>
-                                                    ))}
-                                                </Form.Select>
-                                            </Form.Group>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <div className="py-4">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-5">
-                            <div className="card">
-                                <div className="card-header">
-                                    <h4>Order Details</h4>
-                                </div>
-                                <div className="card-body">
-                                    <div className="row">
-                                        <div className="col-md-6">
-                                            <Stack gap={3}>
-                                                <VStack padding={10} rowGap={5}>
-                                                    {cartItems.map((item) => (
-                                                        <CheckoutItem
-                                                            key={item.name}
-                                                            {...item}
-                                                        />
-                                                    ))}
-                                                    <div className="ms-auto fw-bold fs-6">
-                                                        <Divider orientation="vertical" />
-                                                        Total:&nbsp;
-                                                        {formatMoney(
-                                                            cartItems.reduce(
-                                                                (
-                                                                    total,
-                                                                    cartItem
-                                                                ) => {
-                                                                    let item =
-                                                                        prodM.find(
-                                                                            (
-                                                                                item
-                                                                            ) =>
-                                                                                item.name ===
-                                                                                cartItem.name
-                                                                        );
-                                                                    if (
-                                                                        item ==
-                                                                        null
-                                                                    ) {
-                                                                        item =
-                                                                            prodW.find(
-                                                                                (
-                                                                                    item
-                                                                                ) =>
-                                                                                    item.name ===
-                                                                                    cartItem.name
-                                                                            );
-                                                                    }
-                                                                    return (
-                                                                        total +
-                                                                        (item?.price ||
-                                                                            0) *
-                                                                            cartItem.quantity
-                                                                    );
-                                                                },
-                                                                0
-                                                            )
-                                                        )}
-                                                    </div>
-                                                </VStack>
-                                            </Stack>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            <PaymentInformation></PaymentInformation>
+            <OrderConfirmation
+                Fname={Fname}
+                Lname={Lname}
+                email={email}
+                number={number}
+                addr1={addr1}
+                addr2={addr2}
+                city={city}
+                state={state}
+                zip={zip}
+            ></OrderConfirmation>
         </div>
     );
 };
