1bdc9dde9b75073d1962042ed555ef7df928d324
pkstoy13 <pkstoyanov1@gmail.com>
2023-05-20 00:39:31 -0400

    ProductList.test.tsx baiscally done

--- src/components/ProductList.test.tsx
+++ src/components/ProductList.test.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { render, screen } from "@testing-library/react";
-import ProductList from "./ProductList";
+import { render, screen, fireEvent } from "@testing-library/react";
+import ProductList, { prodM } from "./ProductList";
 import { ChakraProvider } from "@chakra-ui/react";
 import { ShoppingCartProvider } from "../context/ShoppingCartContext";
 import "@testing-library/jest-dom";
@@ -16,5 +16,41 @@ describe("Testing ProductList Component", () => {
                 </ChakraProvider>
             </React.StrictMode>
         );
+    });
+    beforeEach(() => {
+        render(
+            <React.StrictMode>
+                <ChakraProvider>
+                    <ShoppingCartProvider>
+                        <ProductList />
+                    </ShoppingCartProvider>
+                </ChakraProvider>
+            </React.StrictMode>
+        );
+    });
+    test("There is more than 1 Remove Button", () => {
+        const remove = screen.getAllByRole("button", { name: /Remove/i });
+        expect(remove.length).toBeGreaterThan(1);
+    });
+    test("Initial selected filter is 'All' for product list", () => {
+        const selectedFilter = screen.getByRole("button", { name: "All" });
+        expect(selectedFilter).toBeInTheDocument();
+    });
+    test("Testing to see if a product is recognized", () => {
+        const expectedProduct = {
+            name: "UD Football Shirt",
+            price: 10,
+            stock: 10,
+            description: "Support UD Football in this grey t-shirt!",
+            sizes: ["S", "M", "L"],
+            picture: "ud_shirt",
+            category: "Shirts"
+        };
+
+        const foundProduct = prodM.find(
+            (product) => product.name === expectedProduct.name
+        );
+
+        expect(foundProduct).toEqual(expectedProduct);
     });
 });
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -162,6 +162,7 @@ export const removeMProduct = () => {
                 colorScheme="red"
                 disabled={!selectedProduct}
                 onClick={handleRemoveClick}
+                data-testid="remove-button"
             >
                 Remove
             </Button>
