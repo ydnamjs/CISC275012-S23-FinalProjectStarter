abb80f208d47a17d19fc7036e4d06673a9b47ecf
pkstoy13 <pkstoyanov1@gmail.com>
2023-06-02 15:01:24 -0400

    solid start

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,31 @@
 import React from "react";
 import "./App.css";
+import { Button } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
-        <div className="App">
-            <header className="App-header">
-                UD CISC275 with React Hooks and TypeScript
-            </header>
-            <p>
-                Edit <code>src/App.tsx</code> and save. This page will
-                automatically reload.
-            </p>
-        </div>
+        <>
+            <div className="black-space"></div>
+            <div className="App">
+                <header
+                    style={{
+                        fontWeight: "600",
+                        fontSize: "50px",
+                        fontStyle: "oblique"
+                    }}
+                >
+                    About Me
+                </header>
+                <div className="button-style">
+                    <Button variant="outline-dark" size="lg">
+                        LinkedIn
+                    </Button>
+                    <Button variant="outline-dark">Resume</Button>
+                    <Button variant="outline-dark">LeetCode</Button>
+                    <Button variant="outline-dark">Handshake</Button>
+                </div>
+            </div>
+        </>
     );
 }
 
