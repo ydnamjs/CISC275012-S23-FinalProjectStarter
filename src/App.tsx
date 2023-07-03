2da33375a36cc3de9b220d003fa839df784d8cb5
pkstoy13 <pkstoyanov1@gmail.com>
2023-07-02 20:14:07 -0400

    rework

--- src/App.tsx
+++ src/App.tsx
@@ -5,78 +5,102 @@ import { Button, Card } from "react-bootstrap";
 function App(): JSX.Element {
     return (
         <>
-            <div className="App">
-                <header
-                    style={{
-                        fontWeight: "600",
-                        fontSize: "50px",
-                        fontStyle: "oblique"
-                    }}
-                >
-                    About Me
-                </header>
+            <div className="background">
                 <div
+                    className="light-brown"
                     style={{
-                        display: "flex",
-                        flexDirection: "column",
-                        alignItems: "flex-end"
+                        position: "relative",
+                        top: "40px",
+                        left: "40px",
+                        color: "white"
                     }}
                 >
-                    <Card border="light">
-                        <Card.Body style={{ width: "25em", height: "10em" }}>
-                            <Button
-                                variant="dark"
-                                href="https://www.linkedin.com/in/philip-stoyanov-044344229/"
-                                style={{
-                                    width: "25em",
-                                    height: "10em",
-                                    borderLeft: "7px solid orange"
-                                }}
-                            >
-                                LinkedIn
-                            </Button>
-                        </Card.Body>
-                        <Card.Body style={{ width: "25em", height: "10em" }}>
-                            <Button
-                                variant="dark"
-                                href="file:///C:/Users/pksto/OneDrive/Desktop/Philip%20Stoyanov's%20Official%20Resume.pdf"
-                                style={{
-                                    width: "25em",
-                                    height: "10em",
-                                    borderLeft: "7px solid red"
-                                }}
-                            >
-                                Resume
-                            </Button>
-                        </Card.Body>
-                        <Card.Body style={{ width: "25em", height: "10em" }}>
-                            <Button
-                                variant="dark"
-                                href="https://leetcode.com/pkstoy/"
-                                style={{
-                                    width: "25em",
-                                    height: "10em",
-                                    borderLeft: "7px solid salmon"
-                                }}
-                            >
-                                LeetCode
-                            </Button>
-                        </Card.Body>
-                        <Card.Body style={{ width: "25em", height: "10em" }}>
-                            <Button
-                                variant="dark"
-                                href="https://udel.joinhandshake.com/stu/users/33196837"
-                                style={{
-                                    width: "25em",
-                                    height: "10em",
-                                    borderLeft: "7px solid pink"
-                                }}
-                            >
-                                Handshake
-                            </Button>
-                        </Card.Body>
-                    </Card>
+                    Hey!
                 </div>
+                {/*<div className="App">
+                    <header
+                        style={{
+                            fontWeight: "600",
+                            fontSize: "50px",
+                            fontStyle: "oblique",
+                            color: "#f6f2ee"
+                        }}
+                    >
+                        About Me
+                    </header>
+                    
+                    <div
+                        style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "flex-end"
+                        }}
+                    >
+                        <Card border="light">
+                            <Card.Body
+                                style={{ width: "25em", height: "10em" }}
+                            >
+                                <Button
+                                    variant="dark"
+                                    href="https://www.linkedin.com/in/philip-stoyanov-044344229/"
+                                    style={{
+                                        width: "25em",
+                                        height: "10em",
+                                        borderLeft: "7px solid orange"
+                                    }}
+                                >
+                                    LinkedIn
+                                </Button>
+                            </Card.Body>
+                            <Card.Body
+                                style={{ width: "25em", height: "10em" }}
+                            >
+                                <Button
+                                    variant="warning"
+                                    href="file:///C:/Users/pksto/OneDrive/Desktop/Philip%20Stoyanov's%20Official%20Resume.pdf"
+                                    style={{
+                                        width: "25em",
+                                        height: "10em",
+                                        borderLeft: "7px solid red"
+                                    }}
+                                >
+                                    Resume
+                                </Button>
+                            </Card.Body>
+                            <Card.Body
+                                style={{ width: "25em", height: "10em" }}
+                            >
+                                <Button
+                                    variant="dark"
+                                    href="https://leetcode.com/pkstoy/"
+                                    style={{
+                                        width: "25em",
+                                        height: "10em",
+                                        borderLeft: "7px solid salmon"
+                                    }}
+                                >
+                                    LeetCode
+                                </Button>
+                            </Card.Body>
+                            <Card.Body
+                                style={{ width: "25em", height: "10em" }}
+                            >
+                                <Button
+                                    variant="dark"
+                                    href="https://udel.joinhandshake.com/stu/users/33196837"
+                                    style={{
+                                        width: "25em",
+                                        height: "10em",
+                                        borderLeft: "7px solid pink"
+                                    }}
+                                >
+                                    Handshake
+                                </Button>
+                            </Card.Body>
+                        </Card>
+                    </div>
+                    
+                </div>*/}
             </div>
         </>
     );
