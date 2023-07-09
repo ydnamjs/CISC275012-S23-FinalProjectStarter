b7ae84a59cd88517fabbcf96cfa4c8f811c6467e
pkstoy13 <pkstoyanov1@gmail.com>
2023-07-09 18:02:11 -0400

    site up

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import "./App.css";
-import { Button, Card } from "react-bootstrap";
+//import { Button, Card } from "react-bootstrap";
 
 function App(): JSX.Element {
     return (
