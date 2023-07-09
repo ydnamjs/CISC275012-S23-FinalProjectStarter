3db99d29398ada95adf8fc7382ced991aef20565
pkstoy13 <pkstoyanov1@gmail.com>
2023-07-09 17:58:04 -0400

    3 boxes added, 2 done

--- src/App.tsx
+++ src/App.tsx
@@ -7,15 +7,77 @@ function App(): JSX.Element {
         <>
             <div className="background">
                 <div
-                    className="light-brown"
+                    className="clay"
                     style={{
                         position: "relative",
                         top: "40px",
-                        left: "40px",
-                        color: "white"
+                        left: "40px"
                     }}
                 >
-                    Hey!
+                    <div
+                        style={{
+                            position: "relative",
+                            top: "10px",
+                            left: "20px",
+                            whiteSpace: "pre-wrap",
+                            color: "white"
+                        }}
+                    >
+                        <h3>Hey!</h3>
+                        My name is Philip Stoyanov. I am an aspiring Web
+                        Developer. Currently I am a junior attending the
+                        University of Delaware. I am 20 years old. I live in
+                        Wilmington Delaware. I love music, sports. I like to
+                        draw, I like to travel. I like to learn about new things
+                        and talk <br />
+                        to new people. I come from Bulgarian descent. Both of my
+                        parents were born and <br />
+                        raised there. I have friends and grandparents that I
+                        still go visit every 2 years or so
+                    </div>
+                </div>
+                <div
+                    className="happy-green"
+                    style={{
+                        position: "relative",
+                        top: "400px",
+                        left: "-500px"
+                    }}
+                >
+                    <div
+                        style={{
+                            position: "relative",
+                            top: "10px",
+                            left: "20px",
+                            whiteSpace: "pre-wrap",
+                            color: "white"
+                        }}
+                    >
+                        <h3>Motivation</h3>I really did not know what I wanted
+                        to do until my Intro to Software Engineering class. Sure
+                        I was following along in class, learning a lot as I
+                        went, but I didnt really know why.
+                    </div>
+                </div>
+                <div
+                    className="dark-violet"
+                    style={{
+                        position: "relative",
+                        top: "16em",
+                        left: "240px"
+                    }}
+                >
+                    <div
+                        style={{
+                            position: "relative",
+                            top: "10px",
+                            left: "20px",
+                            whiteSpace: "pre-wrap",
+                            color: "white"
+                        }}
+                    >
+                        <h3>Why You Should Hire Me</h3>WIP
+                    </div>
                 </div>
                 {/*<div className="App">
                     <header
